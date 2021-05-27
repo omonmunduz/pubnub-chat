f7c282bc98d85d35b5d6eb00a9f822987918d49c
omonmunduz <omurbekn@gmail.com>
2021-05-27 16:05:52 -0700

    generating random user ids

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import React, { useState, useEffect } from 'react';
     import { Chat, MessageList, MessageInput, ChannelList, MemberList, TypingIndicator } from "@pubnub/react-chat-components";
     import { Picker } from 'emoji-mart';
     import "emoji-mart/css/emoji-mart.css"
+    import { v4 as uuidv4 } from 'uuid';
 
 
     const pubnub = new PubNub({
       publishKey: 'pub-c-9df95d63-a9ee-4b1d-b60f-120f7edad9de',
       subscribeKey: 'sub-c-9808e51e-b9eb-11eb-8f6a-ae5fdf7280c3',
-      uuid: 's'
+      uuid: `${uuidv4()}`
     });
     
     const currentChannel = "myCurrentChannel";
